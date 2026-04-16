a865edab89fa60e7184ded22c4858322c9d9ef78
Programador Cego <programadorcego@gmail.com>
2026-04-16 07:28:47 -0300

    View e Text

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
 export function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Meu texto</Text>
+      <Text>Outro texto</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
