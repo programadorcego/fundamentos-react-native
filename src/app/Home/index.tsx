991a7fdfa91297a8939bd345f925e5495dbe68d0
Programador Cego <programadorcego@gmail.com>
2026-04-16 08:13:19 -0300

    Image

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
@@ -6,6 +6,18 @@ export function Home() {
     <View style={styles.mainContainer}>
       <Text style={{color: "blue", fontSize: 40}}>Meu texto</Text>
       <Text style={styles.text}>Outro texto</Text>
+
+      <Image
+        source={require("../../assets/logo.png")}
+        style={{width: 64, height: 64}}
+        accessibilityLabel="Descrição da Logo"
+      />
+
+      <Image
+        source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }}
+        style={{width: 64, height: 64}}
+        accessibilityLabel="Descrição da Logo"
+      />
     </View>
   );
 }
