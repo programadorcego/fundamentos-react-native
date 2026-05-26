90d2a9295c2200f3ddb257dd913d9ee1e8f27897
Programador Cego <programadorcego@gmail.com>
2026-05-26 07:11:56 -0300

    Componente TextInput

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 import { useState } from "react";
 import { Button } from "@/components/button";
@@ -10,11 +10,27 @@ export function Home() {
     setCount(prev => prev + 1);
   }
 
+  const [text, setText] = useState("");
+  const [number, setNumber] = useState("");
+
   return (
     <View style={styles.mainContainer}>
       <Text>Contador: {count}</Text>
 
       <Button title="Aumentar" onPress={handleIncrease} accessibilityLabel="Clique para aumentar o contador" />
+
+      <TextInput
+        onChangeText={setText}
+        value={text}
+        accessibilityLabel="Nome"
+      />
+
+      <TextInput
+        onChangeText={setNumber}
+        value={number}
+        accessibilityLabel="Idade"
+        keyboardType="numeric"
+      />
     </View>
   );
 }
