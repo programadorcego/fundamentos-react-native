22f122b31ddc1165bf464fde969a65cbaab8cf69
Programador Cego <programadorcego@gmail.com>
2026-06-05 11:17:02 -0300

    Trabalhando com fontes

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,7 +1,27 @@
 import { FlatList, Text, View } from "react-native";
 import { styles } from "./styles";
 
+import { Inter_900Black, useFonts } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync();
+
 export function Home() {
+  const [loaded, error] = useFonts({
+    Inter_900Black,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   const items = Array.from({ length: 50 }, (_, i) => {
     return {
       id: i + 1,
@@ -13,7 +33,7 @@ export function Home() {
     <View style={styles.mainContainer}>
       <FlatList
         data={items}
-        renderItem={({ item }) => <Text>{item.text}</Text>}
+        renderItem={({ item }) => <Text style={{fontFamily: 'Inter_900Black'}}>{item.text}</Text>}
         keyExtractor={item => item.id.toString()}
         contentContainerStyle={styles.flatContainer}
       />
